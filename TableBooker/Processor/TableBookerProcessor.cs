541cae347bba28ad1f9fb050d0cd7f34ad78eb4f
Jose Alejandro Martinez Giral <jalejomg@outlook.com>
2021-04-19 23:42:52 -0500

    Save table booking on data base

--- TableBooker/Processor/TableBookerProcessor.cs
+++ TableBooker/Processor/TableBookerProcessor.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using TableBooker.Repositories;
 
 namespace TableBooker
 {
     public class TableBookerProcessor
     {
+        public ITableBookerRepository _tableBookerRepository { get; }
+        public TableBookerProcessor(ITableBookerRepository tableBookerRepository)
+        {
+            _tableBookerRepository = tableBookerRepository;
+        }
         public TableBooker BookTable(TableBooker request)
         {
             if (request == null) throw new ArgumentNullException("request");
+
+            _tableBookerRepository.Save(request);
 
             return new TableBooker
             {
