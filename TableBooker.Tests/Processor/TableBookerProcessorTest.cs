1a0390373cc4587574beab776294d31fd1263598
Jose Alejandro Martinez Giral <jalejomg@outlook.com>
2021-04-19 22:35:35 -0500

    Create test to based on first requirement
    Data entered on the request must be returned

--- TableBooker.Tests/Processor/TableBookerProcessorTest.cs
+++ TableBooker.Tests/Processor/TableBookerProcessorTest.cs
@@ -1,12 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Xunit;
 
 namespace TableBooker.Tests.Processor
 {
-    class TableBookerProcessorTest
+    public class TableBookerProcessorTest
     {
+        [Fact]
+        public void ShouldReturnTheSameDataEntered()
+        {
+            //Arrange
+            var _request = new TableBooker
+            {
+                Name = "Pedro",
+                LastName = "Díaz",
+                BookerDate = new DateTime(2021, 10, 15),
+                Email = "tekuzeros@tekus.co"
+            };
+
+            var _processor = new TableBookerProcessor();
+
+            //Act
+            var result = _processor.BookTable(_request);
+
+            //Asserts
+            Assert.NotNull(result);
+            Assert.Equal(_request.Name, result.Name);
+            Assert.Equal(_request.LastName, result.LastName);
+            Assert.Equal(_request.BookerDate, result.BookerDate);
+            Assert.Equal(_request.Email, result.Email);
+        }
     }
 }
