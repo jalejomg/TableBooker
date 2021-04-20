d44df58c07cfe46a2cef563f12c0b594bf10e383
Jose Alejandro Martinez Giral <jalejomg@outlook.com>
2021-04-19 22:37:46 -0500

    Create class and method to process table booker request

---
+++ TableBooker/TableBookerProcessor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TableBooker
+{
+    public class TableBookerProcessor
+    {
+        public TableBooker BookTable(TableBooker request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
