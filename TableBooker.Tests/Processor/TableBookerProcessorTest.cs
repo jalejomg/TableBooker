779cf56fa9e00f59987c15f9ffcb6fc8ac5f5a7f
Jose Alejandro Martinez Giral <jalejomg@outlook.com>
2021-04-19 23:20:29 -0500

    Create unit test for 3rd requirement
    Table booking must be saved in database

--- TableBooker.Tests/Processor/TableBookerProcessorTest.cs
+++ TableBooker.Tests/Processor/TableBookerProcessorTest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Moq;
+using System;
+using TableBooker.Repositories;
 using Xunit;
 
 namespace TableBooker.Tests.Processor
@@ -43,6 +45,40 @@ namespace TableBooker.Tests.Processor
 
             //Asserts
             Assert.Equal("request", exception.ParamName);
+        }
+
+        [Fact]
+        public void ShouldSaveTableBookerOnDataBase()
+        {
+            //Arrange
+            var _request = new TableBooker
+            {
+                Name = "Pedro",
+                LastName = "Díaz",
+                BookerDate = new DateTime(2021, 10, 15),
+                Email = "tekuzeros@tekus.co"
+            };
+
+            TableBooker savedTableBooker = null;
+
+            var tableBookerRepository = new Mock<TableBookerRepository>();
+
+            tableBookerRepository.Setup(repository => repository.Save(It.IsAny<TableBooker>()))
+                .Callback<TableBooker>(tableBooker =>
+                    savedTableBooker = tableBooker
+                );
+
+            //Act
+            var result = _processor.BookTable(_request);
+
+            //Asserts
+            tableBookerRepository.Verify(repository => repository.Save(It.IsAny<TableBooker>()), Times.Once);
+
+            Assert.NotNull(result);
+            Assert.Equal(savedTableBooker.Name, result.Name);
+            Assert.Equal(savedTableBooker.LastName, result.LastName);
+            Assert.Equal(savedTableBooker.BookerDate, result.BookerDate);
+            Assert.Equal(savedTableBooker.Email, result.Email);
         }
     }
 }
