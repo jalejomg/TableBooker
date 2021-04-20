35ebfcef87ac06002544e135f3079e5e940a2e72
Jose Alejandro Martinez Giral <jalejomg@outlook.com>
2021-04-19 22:59:56 -0500

    Validate if table booker request is not null
    Throw ArgumentNullException this parameter is null

--- TableBooker/Processor/TableBookerProcessor.cs
+++ TableBooker/Processor/TableBookerProcessor.cs
@@ -1,9 +1,13 @@
-﻿namespace TableBooker
+﻿using System;
+
+namespace TableBooker
 {
     public class TableBookerProcessor
     {
         public TableBooker BookTable(TableBooker request)
         {
+            if (request == null) throw new ArgumentNullException("request");
+
             return new TableBooker
             {
                 Name = request.Name,
