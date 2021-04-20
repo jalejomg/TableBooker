54d97611689f036f4a6092135f7878ee8ec12e53
Jose Alejandro Martinez Giral <jalejomg@outlook.com>
2021-04-19 22:37:03 -0500

    Create model to enter data for table booker request

---
+++ TableBooker/TableBooker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TableBooker
+{
+    public class TableBooker
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime BookerDate { get; set; }
+        public string Email { get; set; }
+    }
+}
